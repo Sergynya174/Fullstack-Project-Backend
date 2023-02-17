3cf96858d5679e2d271d6ba205eed7bae546039e
Sergynya174 <Sergynya174@yandex.ru>
2023-02-17 23:45:54 +0300

    Начало проекта

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from "express";
 import { validationResult } from "express-validator";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 import { registerValidator } from "./validations/auth.js";
+import UserShema from "./models/user.js";
 
 mongoose
   .connect("mongodb://localhost:27017/mydb", {
@@ -29,14 +31,26 @@ app.post("/login", (req, res) => {
   });
 });
 
-app.post("/register", registerValidator, (req, res) => {
+app.post("/register", registerValidator, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
-  res.json({
-    success: true,
+
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserShema({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash: passwordHash,
   });
+
+  const user = await doc.save();
+
+  res.json(user);
 });
 
 app.listen(8080, (err) => {
